b4c484a88a3d699968862968725a972fd9ffb133
liuyb <liuyb@ickey.cn>
2022-06-07 22:11:48 +0800

    go web: JWT

--- user-web/router/user.go
+++ user-web/router/user.go
@@ -5,6 +5,7 @@ import (
 	"go.uber.org/zap"
 
 	"github.com/liuyongbing/hello-go-web/user-web/api"
+	"github.com/liuyongbing/hello-go-web/user-web/middlewares"
 )
 
 func InitUserRouter(Router *gin.RouterGroup) {
@@ -17,6 +18,8 @@ func InitUserRouter(Router *gin.RouterGroup) {
 	// API: User
 	UserRouter := Router.Group("user")
 	{
-		UserRouter.GET("list", api.GetUserList) // 获取用户列表
+		// UserRouter.GET("list", api.GetUserList) // 获取用户列表
+		UserRouter.GET("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), api.GetUserList) // 获取用户列表
+		UserRouter.POST("pwd_login", api.PassWordLogin)                                       // 用户登录
 	}
 }
