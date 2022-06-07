5c7310c1e596ec843e08f2c8c02cfaf27b23546d
liuyb <liuyb@ickey.cn>
2022-06-07 22:22:10 +0800

    ge web: Cross domain

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -3,11 +3,15 @@ package initialize
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/liuyongbing/hello-go-web/user-web/middlewares"
 	"github.com/liuyongbing/hello-go-web/user-web/router"
 )
 
 func Routers() *gin.Engine {
 	r := gin.Default()
+
+	// 配置跨域
+	r.Use(middlewares.Cors())
 
 	ApiGroup := r.Group("/u/v1")
 	router.InitUserRouter(ApiGroup)
