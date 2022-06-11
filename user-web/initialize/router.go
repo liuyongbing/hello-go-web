3c923e8a9709b6c020d185b9ce123a2438dc7350
liuyb <liuyb@ickey.cn>
2022-06-11 10:14:55 +0800

    go web: route for consul health check

--- user-web/initialize/router.go
+++ user-web/initialize/router.go
@@ -1,14 +1,41 @@
 package initialize
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/liuyongbing/hello-go-web/user-web/middlewares"
 	"github.com/liuyongbing/hello-go-web/user-web/router"
 )
 
+/*
+pong
+default response for get "/"
+*/
+func pong(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, Welcome to go web.",
+		"time":    time.Now(),
+	})
+}
+
+/*
+health
+服务的健康检查
+*/
+func health(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, service is healthly.",
+		"time":    time.Now(),
+	})
+}
+
 func Routers() *gin.Engine {
 	r := gin.Default()
+	r.GET("/", pong)
+	r.GET("/health", health)
 
 	// 配置跨域
 	r.Use(middlewares.Cors())
